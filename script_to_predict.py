dc43a318af86af9c39949ad7b918ff3bde143ef8
ErlingAmundsen <94839025+ErlingAmundsen@users.noreply.github.com>
2023-03-26 21:39:05 +0200

    Generates a file with predictions

--- script_to_predict.py
+++ script_to_predict.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import codecs
 import pickle  as pickle
+from transformers import AutoModelForTokenClassification, AutoTokenizer
 
 data = []
 current_words = []
 current_tags = []
+labels = []
+
+
 
 for line in codecs.open('data/baseline/en_ewt_nn_answers_test.conll', encoding='utf-8'):
     line = line.strip()
@@ -26,36 +30,50 @@ for line in codecs.open('data/baseline/en_ewt_nn_answers_test.conll', encoding='
         current_tags = []
 
 
+names=[
+                    "O",
+                    "B-PER",
+                    "I-PER",
+                    "B-ORG",
+                    "I-ORG",
+                    "B-LOC",
+                    "I-LOC",
+                    "B-MISC",
+                    "I-MISC",
+                ]
+
+## Find all the unique labels in the data
+
+
 ## NAME CSV WHAT YOU WANT AND GIVE IT A PATH MABY??
 outF = open("baseline.txt", "w")
 
 
-## Load the model here
-model = pickle.load(open('model.pkl', 'rb'))
+model = AutoModelForTokenClassification.from_pretrained("models/bert-base-multilingual-cased-finetuned-ner")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+
+## get all the unique labels from the data in a list
+
 
 for i in data:
     sent = i[0]
-    label = i[1]
-    
-    ## Do some fancy predictioning here
-    
-    predictions = model.argmax(sentance)
-    
-    ## remapping 
-    
-    predicted_token_class = [model.config.id2label[t.item()] for t in predictions[0]]
-    
-    ## pred = predictions made for each word in sent
-    
-    for index in range(len(sent)):
+
+    pa = tokenizer.encode(' '.join(sent), return_tensors="pt", add_special_tokens=False)
+    predictions = model(pa)
+
+    for index in range(1, len(sent) + 1):
+        pred = predictions.logits[0][index].argmax().item()
         
+        
+        print(pred)
+
         ## HERE word[index] is the original word from the sentanve, and pred[index] is the predicted label for that
-        out = sent[index] + '\t' + pred[index]
+        out = sent[index-1] + '\t' + names[pred]
 
         # wrtie line to output file
         outF.write(out)
         outF.write("\n")
-        
+    break
     out = '\t'
     outF.write(out)
     outF.write("\n")
