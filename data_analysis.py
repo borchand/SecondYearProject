081a450af23e27538441856951d8d6e24bc10d81
Sebastian <andersen_sebastian@yahoo.dk>
2023-05-17 21:10:23 +0200

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -3,22 +3,20 @@ import numpy as np
 from collections import Counter
 from utils import read_conll
 
-english_paths  = {'train': 'data/datasets/baseline/en_ewt_nn_train.conll', 
-                  'validation': 'data/datasets/baseline/en_ewt_nn_newsgroup_test.conll', 
-                  'test': 'data/datasets/baseline/en_ewt_nn_newsgroup_dev.conll'}
-
-german_paths = {"train": "data/datasets/NoSta-D/NER-de-train.tsv",
-                "validation": "data/datasets/NoSta-D/NER-de-test.tsv",
-                "test": "data/datasets/NoSta-D/NER-de-dev.tsv",}
-
-danish_paths = { "train": "data/datasets/DaNplus/da_news_train.tsv",
-                "validation": "data/datasets/DaNplus/da_news_test.tsv",
-                "test": "data/datasets/DaNplus/da_news_dev.tsv",}
+paths  = {'en': ['data/datasets/baseline/en_ewt_nn_train.conll', 'data/datasets/baseline/en_ewt_nn_newsgroup_test.conll', 'data/datasets/baseline/en_ewt_nn_newsgroup_dev.conll'], 
+                  'ger': "data/datasets/NoSta-D/NER-de-test.tsv", 
+                  'dk': "data/datasets/DaNplus/da_news_test.tsv"}
 
 
-def get_data(path):
-    _, labels = read_conll(path)
-    labels = [item for item in labels for item in item]
+def get_data(paths):
+    if type(paths) == list:
+        labels = []
+        for path in paths:
+            _, l = read_conll(path)
+            labels += l
+    else:
+        _, labels = read_conll(paths)
+    labels = [item.replace("B-", "").replace("I-", "") for item in labels for item in item]
     count = Counter(labels)
     sum_ = sum(count.values())
     ratio  = {}
@@ -47,5 +45,6 @@ def all_datasets(paths, specific = False):
 
         
 if __name__ == '__main__':
-    print(all_datasets(english_paths))
+    # print(pd.DataFrame.from_dict(all_datasets(english_paths)).to_latex())
+    print(all_datasets(paths))
     ## get a dictionary of type {train:{count:{}, ratio:{}}}
